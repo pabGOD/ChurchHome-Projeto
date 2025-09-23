e346568c3263c50c5a92bdfed2cce741c6845640
pabGOD <pabllo.freitas@hotmail.com>
2025-09-23 10:49:28 -0300

    projeto final b

---
+++ church/src/app/cultos/cultos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cultos',
+  imports: [],
+  templateUrl: './cultos.component.html',
+  styleUrl: './cultos.component.css'
+})
+export class CultosComponent {
+
+}
